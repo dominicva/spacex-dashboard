aa4186f1d136d3ace976a920e2833c1a1baa59c6
Dominic <dominicvana@gmail.com>
2021-04-13 11:49:20 +0100

    Rendering company summary

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 const SPACEX_API = {
   BASE_URL: 'https://api.spacexdata.com/v4',
-  ENDPOINTS: {
-    COMPANY: '/company',
-    LATEST_LAUNCH: '/launches/latest',
-    NEXT_LAUNCH: '/launches/next',
-  },
+  COMPANY: '/company',
+  LATEST_LAUNCH: '/launches/latest',
+  NEXT_LAUNCH: '/launches/next',
 };
+
+const getCompanyInfo = function ({ BASE_URL, COMPANY }) {
+  return fetch(`${BASE_URL}${COMPANY}`)
+    .then((data) => data.json())
+    .then(
+      (parsedData) =>
+        (document.getElementById('summary').textContent = parsedData.summary)
+    )
+    .catch((err) => {
+      console.log('Something went wrong getting company info', err);
+    });
+};
+
+// const onPageLoad = function () {
+// };
+
+window.addEventListener(
+  'DOMContentLoaded',
+  getCompanyInfo.bind(this, SPACEX_API)
+);
