67ea655f977201a62aed9d997306f396118827a7
Dominic <dominicvana@gmail.com>
2021-04-13 11:32:33 +0100

    API endpoints added to index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const SPACEX_API = {
+  BASE_URL: 'https://api.spacexdata.com/v4',
+  ENDPOINTS: {
+    COMPANY: '/company',
+    LATEST_LAUNCH: '/launches/latest',
+    NEXT_LAUNCH: '/launches/next',
+  },
+};
